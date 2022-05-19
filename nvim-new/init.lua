cbfb0c513425729af2c1401f2ced67c7d96b21f6
Henry Cortez <henry200amaya@gmail.com>
2022-05-19 15:06:35 -0600

    :sparkles: Add new config

---
+++ nvim-new/init.lua
@@ -0,0 +1,23 @@
+pcall(require, "impatient")
+
+if require "hencor.first_load"() then
+  return
+end
+
+vim.g.mapleader = ","
+
+vim.g.snippets = "luasnip"
+
+require "hencor.globals"
+
+require "hencor.disable_builtin"
+
+require "hencor.plugins"
+
+require "hencor.telescope.setup"
+require "hencor.telescope.mappings"
+require "hencor.nvim-tree"
+-- require "hencor.dap"
+
+require "hencor.refactor"
+require "hencor.lsp"
